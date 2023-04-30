2249d17928edf9530cf6ca5b31f353c0c63283b5
dawood <dawood.altai111@gmail.com>
2023-04-30 14:02:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  return array.reduce((foundPair, currentValue, currentIndex) => {
+    const complement = target - currentValue;
+    const restOfArray = array.slice(currentIndex + 1);
+    const foundInRest = restOfArray.includes(complement);
+    return foundPair || foundInRest;
+  }, false);
 }
 
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10))
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+1:Loop through the input array from index 0 to n-1.
+2:For each iteration, subtract the current value from the target to get the complement.
+3:Slice the rest of the array from the current index + 1 to n-1.
+4:Check if the complement exists in the rest of the array using the includes method.
+5:If the complement is found, return true, indicating that a pair with the target sum exists.
+6:If the loop completes and no pair is found, return false.
 */
 
 /*
-  Add written explanation of your solution here
+  The function takes in an array and a target number and checks if there are any two numbers in the array that add up to the target. The implementation achieves this by using a nested loop that compares each element in the array to all other elements after it to check if their sum is equal to the target.
 */
 
 // You can run `node index.js` to view these console logs
